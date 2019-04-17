26fdfccec1d8cd874dc927f925c504581516028c
daviddmr <david.dmr@gmail.com>
2019-04-17 03:23:09 -0300

    Including logging interceptor to web requests

--- app/src/main/java/com/arctouch/codechallenge/injection/module/RetrofitModule.kt
+++ app/src/main/java/com/arctouch/codechallenge/injection/module/RetrofitModule.kt
@@ -5,20 +5,28 @@ import com.arctouch.codechallenge.api.TmdbApi
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
+
 @Module
 class RetrofitModule {
 
+    private val logging: HttpLoggingInterceptor = HttpLoggingInterceptor()
     private val httpClient: OkHttpClient.Builder = OkHttpClient.Builder()
 
     private val builder: Retrofit.Builder = Retrofit.Builder()
             .baseUrl(BuildConfig.API_URL)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(MoshiConverterFactory.create())
+
+    init {
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        httpClient.addInterceptor(logging)
+    }
 
     @Provides
     @Singleton
