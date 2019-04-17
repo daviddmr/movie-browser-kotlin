209febac31465a682c907325986d54d1173de59e
daviddmr <david.dmr@gmail.com>
2019-04-16 21:10:28 -0300

    Including parcelize to models movie and genre

--- app/src/main/java/com/arctouch/codechallenge/model/Models.kt
+++ app/src/main/java/com/arctouch/codechallenge/model/Models.kt
@@ -1,10 +1,13 @@
 package com.arctouch.codechallenge.model
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
 data class GenreResponse(val genres: List<Genre>)
 
-data class Genre(val id: Int, val name: String)
+@Parcelize
+data class Genre(val id: Int, val name: String) : Parcelable
 
 data class UpcomingMoviesResponse(
     val page: Int,
@@ -13,13 +16,13 @@ data class UpcomingMoviesResponse(
     @Json(name = "total_results") val totalResults: Int
 )
 
+@Parcelize
 data class Movie(
-    val id: Int,
-    val title: String,
-    val overview: String?,
-    val genres: List<Genre>?,
-    @Json(name = "genre_ids") val genreIds: List<Int>?,
-    @Json(name = "poster_path") val posterPath: String?,
-    @Json(name = "backdrop_path") val backdropPath: String?,
-    @Json(name = "release_date") val releaseDate: String?
-)
+        val id: Int,
+        val title: String?,
+        val overview: String?,
+        val genres: List<Genre>?,
+        @Json(name = "genre_ids") val genreIds: List<Int>?,
+        @Json(name = "poster_path") val posterPath: String?,
+        @Json(name = "backdrop_path") val backdropPath: String?,
+        @Json(name = "release_date") val releaseDate: String?) : Parcelable
