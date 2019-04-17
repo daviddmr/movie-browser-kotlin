ff7a5d68ed825103a470507310d85b4633f15a28
daviddmr <david.dmr@gmail.com>
2019-04-16 23:46:06 -0300

    Changing the method to build the url of pictures from the movies

--- app/src/main/java/com/arctouch/codechallenge/util/MovieImageUrlBuilder.kt
+++ app/src/main/java/com/arctouch/codechallenge/util/MovieImageUrlBuilder.kt
@@ -2,16 +2,16 @@ package com.arctouch.codechallenge.util
 
 import com.arctouch.codechallenge.api.TmdbApi
 
-private val POSTER_URL = "https://image.tmdb.org/t/p/w154"
-private val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
+private const val POSTER_URL = "https://image.tmdb.org/t/p/w154"
+private const val BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
 
 class MovieImageUrlBuilder {
 
     fun buildPosterUrl(posterPath: String): String {
-        return POSTER_URL + posterPath + "?api_key=" + TmdbApi.API_KEY
+        return "$POSTER_URL$posterPath?api_key=${TmdbApi.API_KEY}"
     }
 
     fun buildBackdropUrl(backdropPath: String): String {
-        return BACKDROP_URL + backdropPath + "?api_key=" + TmdbApi.API_KEY
+        return "$BACKDROP_URL$backdropPath?api_key=${TmdbApi.API_KEY}"
     }
 }
