7258c36ae9202efde9311959bc7f24ff373ea03f
daviddmr <david.dmr@gmail.com>
2019-04-18 01:09:00 -0300

    Putting information when fields are empty

--- app/src/main/java/com/arctouch/codechallenge/util/CustomBindingAdapter.java
+++ app/src/main/java/com/arctouch/codechallenge/util/CustomBindingAdapter.java
@@ -3,11 +3,16 @@ package com.arctouch.codechallenge.util;
 import android.databinding.BindingAdapter;
 import android.widget.ImageView;
 
+import com.arctouch.codechallenge.R;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 
 public class CustomBindingAdapter {
     @BindingAdapter({"bind:image_url"})
     public static void loadImage(ImageView imageView, String url) {
-        Glide.with(imageView.getContext()).load(url).into(imageView);
+        Glide.with(imageView.getContext())
+                .load(url)
+                .apply(new RequestOptions().placeholder(R.drawable.ic_image_placeholder))
+                .into(imageView);
     }
 }
