cdd321b9cace87ce960d01273d56cab200e8c2d0
daviddmr <david.dmr@gmail.com>
2020-04-10 02:23:05 -0400

    Create Event class to handle with wrapped single live events

---
+++ app/src/main/java/com/arctouch/codechallenge/util/Event.kt
@@ -0,0 +1,53 @@
+package com.arctouch.codechallenge.util
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+
+/**
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ */
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peekContent(): T = content
+}
+
+/*How to use:
+viewModel.<name_of_observable>.observeEvent(this) { value ->
+    //Do something with the value
+}
+*/
+inline fun <T> LiveData<Event<T>>.observeEvent(owner: LifecycleOwner, crossinline onEventUnhandledContent: (T) -> Unit) {
+    observe(owner, Observer { it?.getContentIfNotHandled()?.let(onEventUnhandledContent) })
+}
+
+/*
+How to use:
+viewModel.<name_of_observable>.observe(this, EventObserver { value ->
+    //Do something with the value
+})
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotHandled()?.let { value ->
+            onEventUnhandledContent(value)
+        }
+    }
+}*/
