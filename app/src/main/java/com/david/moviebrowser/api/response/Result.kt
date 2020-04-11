11b8ea559e5ff7d5539c15ae2c9d9f8b949d4795
daviddmr <david.dmr@gmail.com>
2020-04-11 00:58:30 -0400

    Create Result class to handle with onSuccess and onError responses from requests

---
+++ app/src/main/java/com/david/moviebrowser/api/response/Result.kt
@@ -0,0 +1,6 @@
+package com.david.moviebrowser.api.response
+
+sealed class Result<out T: Any> {
+    data class Success<out T : Any>(val data: T) : Result<T>()
+    data class Error(val exception: String) : Result<Nothing>()
+}
