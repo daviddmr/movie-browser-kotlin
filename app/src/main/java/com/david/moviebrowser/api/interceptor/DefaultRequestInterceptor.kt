a2b399285c271b55df27ac0f31eb858b1af83de1
David Ribeiro <david.dmr@gmail.com>
2024-04-09 16:53:49 -0400

    Code clean in DefaultRequestInterceptor

--- app/src/main/java/com/david/moviebrowser/api/interceptor/DefaultRequestInterceptor.kt
+++ app/src/main/java/com/david/moviebrowser/api/interceptor/DefaultRequestInterceptor.kt
@@ -6,20 +6,20 @@ import com.david.moviebrowser.injection.module.RetrofitModule
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class DefaultRequestInterceptor: Interceptor {
+class DefaultRequestInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
         val originalHttpUrl = original.url
 
         val url = originalHttpUrl.newBuilder()
-                .addQueryParameter("api_key", RetrofitModule.API_KEY)
-                .addQueryParameter("language", App.res.getString(R.string.default_language))
-                .addQueryParameter("region", App.res.getString(R.string.default_region))
-                .build()
+            .addQueryParameter("api_key", RetrofitModule.API_KEY)
+            .addQueryParameter("language", App.res.getString(R.string.default_language))
+            .addQueryParameter("region", App.res.getString(R.string.default_region))
+            .build()
 
         val requestBuilder = original.newBuilder()
-                .url(url)
+            .url(url)
 
         val request = requestBuilder.build()
         return chain.proceed(request)
