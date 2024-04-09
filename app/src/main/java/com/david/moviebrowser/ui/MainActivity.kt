12f14d30994d78f0d1c5b416cbccb9a7e28e93ed
David Ribeiro <david.dmr@gmail.com>
2024-04-08 20:30:39 -0400

    Remove appbar layout from main activity layout

--- app/src/main/java/com/david/moviebrowser/ui/MainActivity.kt
+++ app/src/main/java/com/david/moviebrowser/ui/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
 
         val fragment = HomeFragment.newInstance()
         supportFragmentManager
